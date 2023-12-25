f76798d10b3473ed19d44b8e272141c2379a57d3
Alexey <a504512@mail.ru>
2023-12-25 21:20:01 +0300

    first commit

---
+++ web/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpRequest
+from django.shortcuts import render
+from web.models import Product
+
+# Create your views here.
+def index(request: HttpRequest):
+  products = Product.objects.all() #взять все товары
+  context = {"products": products}
+
+  return render(request, "index.html", context=context)
+
+def contacts(request: HttpRequest):
+  return render(request, "contacts.html")
+
